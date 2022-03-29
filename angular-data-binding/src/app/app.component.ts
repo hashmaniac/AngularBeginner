275cd93fbb269593775bad04ec2f0e83d26416a7
hashmaniac <mbithidenis3@gmail.com>
2022-03-29 16:22:02 +0300

    data-binding

--- angular-data-binding/src/app/app.component.ts
+++ angular-data-binding/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-data-binding';
+
+  hello = 'Hello World';
+  class = 'centered';
+  jina = 'name';
+
+  onSubmit() {
+    alert('Welcome ' + this.jina + "!");
+  }
+
 }
